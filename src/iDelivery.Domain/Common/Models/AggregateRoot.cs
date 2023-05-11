4bd614070f50840391f10930f2d5af694bda4ab6
Djoufson <djouflegran@gmail.com>
2023-05-11 05:23:46 -0700

    Implementation des bases AggregateRoot, Entity et ValueObject

---
+++ src/iDelivery.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,9 @@
+﻿namespace iDelivery.Domain.Common.Models;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id)
+    {
+    }
+}
---
+++ src/iDelivery.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,37 @@
+﻿namespace iDelivery.Domain.Common.Models;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (obj is not ValueObject other) return false;
+
+        return GetEqualityComponents()
+            .SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x?.GetHashCode() ?? 0)
+            .Aggregate((acc, x) => acc ^ x)
+            .GetHashCode();
+    }
+
+    public bool Equals(ValueObject? other)
+    {
+        return Equals((object?) other);
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right)
+    {
+        return Equals(left, right);
+    }
+    public static bool operator !=(ValueObject left, ValueObject right)
+    {
+        return !Equals(left, right);
+    }
+}
