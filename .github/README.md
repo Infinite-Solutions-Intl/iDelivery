e74a5479e725ed73f880a9536a4136db543aafbe
Djoufson <djouflegran@gmail.com>
2023-05-13 03:54:27 -0700

    Ajout des packages

--- .github/README.md
+++ .github/README.md
@@ -1,2 +1,2 @@
-# Workflowa
+# Workflows
 Ici seront renseignés les differents automatismes que l'on souhaite trigger via les GitHub Actions
--- src/iDelivery.Api/Program.cs
+++ src/iDelivery.Api/Program.cs
@@ -1,3 +1,6 @@
+using iDelivery.Application;
+using iDelivery.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Layers
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
